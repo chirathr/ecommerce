a85234f2ce07e9871d3a814ede3b2ee4f6196e7b
Chirath R <chirath.02@gmail.com>
2018-10-04 13:37:25 +0530

    django-hamlpy template preprocessor

--- website/settings.py
+++ website/settings.py
@@ -57,8 +57,11 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
-        'APP_DIRS': True,
         'OPTIONS': {
+            'loaders': [
+                'hamlpy.template.loaders.HamlPyFilesystemLoader',
+                'hamlpy.template.loaders.HamlPyAppDirectoriesLoader',
+            ],
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
