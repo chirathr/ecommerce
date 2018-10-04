30a3008f24a33da22dc008a48f33732d7dfb444e
Chirath R <chirath.02@gmail.com>
2018-10-04 14:28:11 +0530

    Setup for django-allauth

--- website/settings.py
+++ website/settings.py
@@ -37,7 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+
+    # Third party
     'hamlpy',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
+
+    # My apps
     'registration',
     'ecommerce',
 ]
@@ -127,3 +136,14 @@ STATIC_URL = '/static/'
 
 
 AUTH_USER_MODEL = 'registration.User'
+
+
+AUTHENTICATION_BACKENDS = (
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+SITE_ID = 1
