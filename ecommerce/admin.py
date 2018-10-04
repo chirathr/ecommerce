b458badbc81af6d5db8ac74a712ece9f795096a8
Chirath R <chirath.02@gmail.com>
2018-10-04 15:26:48 +0530

    migrations and admin settings

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from ecommerce.models import Image, Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    fields = (
+        ('name', 'quantity'),
+        'description',
+        ('price', 'discount_percent'),
+        'rating'
+    )
+
+    list_display = ('name', 'price', 'rating')
+
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'product', 'featured_image')
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Image, ImageAdmin)
--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    """
+    Describes each product
+    """
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, default="")
+    price = models.FloatField(default=0.0)
+    discount_percent = models.FloatField(default=0.0)
+    rating = models.IntegerField(blank=True, null=True)
+    quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class Image(models.Model):
+    """
+    Image associated with a product
+    """
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    image_path = models.ImageField(upload_to='products/')
+    featured_image = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "{0} - {1}".format(self.product.name, self.name)
