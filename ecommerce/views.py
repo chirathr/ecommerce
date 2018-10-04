cc7301f239e189a9445d17b49c8ef8980657092e
Chirath R <chirath.02@gmail.com>
2018-10-04 13:53:34 +0530

    Got django haml working

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import TemplateView
+from hamlpy.views.generic import HamlExtensionTemplateView
+
+
+class Home(HamlExtensionTemplateView, TemplateView):
+    template_name = 'base/base.haml'
