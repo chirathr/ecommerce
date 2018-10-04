247a44a184b81376a3ce9870afd7940fd7a7ef98
Chirath R <chirath.02@gmail.com>
2018-10-04 13:32:48 +0530

    Custom User Model to support wallet balance

--- registration/admin.py
+++ registration/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import User
+
+
+class UserAdmin(BaseUserAdmin):
+    model = User
+    fieldsets = BaseUserAdmin.fieldsets + ((_('Wallet Information'), {'fields': ('wallet_balance', )}), )
+
+
+admin.site.register(User, UserAdmin)
