0b48aae4797a82f84e3194deb3ed8620826bb6e6
Chirath R <chirath.02@gmail.com>
2018-10-05 15:07:01 +0530

    Load user data command

---
+++ registration/management/commands/load_user_data.py
@@ -0,0 +1,31 @@
+import requests
+import json
+
+from django.core.management.base import BaseCommand, CommandError
+from registration.models import User
+
+
+class Command(BaseCommand):
+    help = 'Populate db with product data from fixture'
+
+    def handle(self, *args, **options):
+
+        r = requests.get('https://uinames.com/api/?amount=20')
+        user_data_json = json.loads(r.text)
+
+        for user_data in user_data_json:
+            username = str(user_data['name']).lower()
+            if User.objects.filter(username=username).count() > 0:
+                break
+
+            first_name = str(user_data['name'])
+            last_name = str(user_data['surname'])
+
+            User.objects.create_user(
+                username=username,
+                email="{0}@gmail.com".format(username),
+                password="qwerty@123",
+                first_name=first_name,
+                last_name=last_name
+
+            )
--- registration/views.py
+++ registration/views.py
@@ -1,9 +1,5 @@
-from django.shortcuts import render
-
 from hamlpy.views.generic import HamlExtensionTemplateView
 from allauth.account.views import LoginView as BaseLoginView
-
-# Create your views here.
 
 
 class LoginView(HamlExtensionTemplateView, BaseLoginView):
