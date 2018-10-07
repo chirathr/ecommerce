ffe9c9ce5044d885dc5055ddae0ce86448f03036
Chirath R <chirath.02@gmail.com>
2018-10-07 20:18:35 +0530

    Add to cart works

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from ecommerce.views import ProductListView,UserCartListView
+from ecommerce.views import ProductListView, UserCartListView, UserCartAddView
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
-    path('cart/', UserCartListView.as_view(), name='cart_list'),
+    path('cart/', UserCartListView.as_view(), name='cart'),
+    path('cart/add/', UserCartAddView.as_view(), name='add_to_cart'),
 ]
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,4 +1,7 @@
+from django.http import Http404
+from django.shortcuts import redirect
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from hamlpy.views.generic import DetailView, ListView
 
 from ecommerce.models import Product, Cart
@@ -13,7 +16,7 @@ class ProductDetailView(DetailView):
     model = Product
 
 
-class UserCartListView(TemplateView):
+class UserCartListView(LoginRequiredMixin, TemplateView):
     template_name = 'ecommerce/cart.html.haml'
     model = Cart
 
@@ -21,3 +24,30 @@ class UserCartListView(TemplateView):
         context = super(UserCartListView, self).get_context_data(**kwargs)
         context['cart_list'] = Cart.objects.filter(user=self.request.user)
         return context
+
+
+class UserCartAddView(LoginRequiredMixin, TemplateView):
+    success_url = 'cart'
+    model = Cart
+
+    def get(self, request, *args):
+        return redirect('home')
+
+    def post(self, request, *args):
+        if 'product' in request.POST:
+            try:
+                product = Product.objects.get(pk=int(request.POST['product']))
+                if Cart.objects.filter(user=request.user, product=product).count() > 0:
+                    cart_item = Cart.objects.get(user=request.user, product=product)
+                    cart_item.quantity += 1
+                    cart_item.save()
+                else:
+                    Cart.objects.create(user=request.user, product=product, quantity=1)
+            except ValueError:
+                raise Http404
+            except Product.DoesNotExist:
+                raise Http404
+        else:
+            raise Http404
+
+        return redirect(self.success_url)
