f8522a995684fff0dc0b6ed4651339c8bc842c76
Chirath R <chirath.02@gmail.com>
2018-10-07 18:30:34 +0530

    Cart model

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
 
+from registration.models import User
+
 
 class ProductCategory(models.Model):
     """
@@ -62,3 +64,14 @@ class Image(models.Model):
 
     def __str__(self):
         return "{0} - {1}".format(self.product.name, self.name)
+
+
+class Cart(models.Model):
+    """
+    Shopping cart for users to add products
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{0} - {1}".format(self.user.username, self.product.name)
