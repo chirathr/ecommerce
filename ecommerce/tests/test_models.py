62fdf44ee5bd102ccad543548961495a344e5ff2
Chirath R <chirath.02@gmail.com>
2018-10-08 14:50:41 +0530

    Tests for cart model

--- ecommerce/tests/test_models.py
+++ ecommerce/tests/test_models.py
@@ -1,12 +1,13 @@
 from django.test import TestCase
 
-from ecommerce.models import Product, Image, ProductCategory
+from ecommerce.models import Product, Image, ProductCategory, Cart, Order, OrderList
+from registration.models import User
 
 
 class TestProductModel(TestCase):
     def setUp(self):
-        Product.objects.create(
-            name="Name", price=50.0, discount_percent=9.99, quantity=10)
+        self.product = Product.objects.create(
+            name="Name", price=50.0, discount_percent=10, quantity=10, rating=4)
 
     def test_object_name_is_product_name(self):
         product = Product.objects.first()
@@ -34,6 +35,18 @@ class TestProductModel(TestCase):
         expected_images = (image_1.image_path, image_2.image_path)
         self.assertIn(product.featured_image, expected_images)
 
+    def test_discounted_price(self):
+        discounted_price = 45
+        self.assertEqual(discounted_price, self.product.discount_price)
+
+    def test_starts(self):
+        self.product.rating = 3
+        self.assertEqual(range(3), self.product.stars)
+
+    def test_empty_start(self):
+        self.product.rating = 1
+        self.assertEqual(range(4), self.product.stars_empty)
+
 
 class TestImageModel(TestCase):
     def setUp(self):
@@ -55,4 +68,18 @@ class TestProductCategoryModel(TestCase):
     def test_object_name_is_category_name(self):
         product_category = ProductCategory.objects.first()
         self.assertEqual("Category 1", str(product_category))
+
+
+class TestCartModel(TestCase):
+    def setUp(self):
+        self.product = Product.objects.create(
+            name="Name", price=50.0, discount_percent=10, quantity=10, rating=4)
+        self.user = User.objects.create_user(username="name", password="password")
+        self.cart = Cart.objects.create(user=self.user, product=self.product, quantity=1)
+
+    def test_object_name_username_hyphen_product_name(self):
+        cart = Cart.objects.first()
+        expected_name = "{0} - {1}".format(self.user.username, self.product.name)
+        self.assertEqual(expected_name, str(cart))
+
 
