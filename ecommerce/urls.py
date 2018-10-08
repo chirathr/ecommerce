3ace7204f467139cdacc4d95d38c8d05cc9b9e60
Chirath R <chirath.02@gmail.com>
2018-10-08 10:37:12 +0530

    Check product quantity when adding to cart

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from ecommerce.views import ProductListView, UserCartListView, UserCartAddView, UserCartDeleteView
 
@@ -6,5 +7,7 @@ urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
     path('cart/', UserCartListView.as_view(), name='cart'),
     path('cart/add/', UserCartAddView.as_view(), name='add_to_cart'),
+    path('cart/add/failed/',
+         TemplateView.as_view(template_name='ecommerce/cannot_add_to_cart.html.haml'), name='cannot_add_to_cart'),
     path('cart/delete/', UserCartDeleteView.as_view(), name='delete_from_cart'),
 ]
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -33,6 +33,7 @@ class UserCartListView(LoginRequiredMixin, TemplateView):
 
 class UserCartAddView(LoginRequiredMixin, TemplateView):
     success_url = 'cart'
+    cannot_add_to_cart_url = 'cannot_add_to_cart'
 
     def get(self, request, *args):
         return redirect('home')
@@ -40,19 +41,31 @@ class UserCartAddView(LoginRequiredMixin, TemplateView):
     def post(self, request, *args):
         if 'product' in request.POST:
             try:
+                # Get the product that the user is trying to add to cart
                 product = Product.objects.get(pk=int(request.POST['product']))
+
+                # check if product is available
+                if product.quantity == 0:
+                    return redirect(self.cannot_add_to_cart_url)
+
+                # if already there in cart add quantity
                 if Cart.objects.filter(user=request.user, product=product).count() > 0:
                     cart_item = Cart.objects.get(user=request.user, product=product)
+                    if cart_item.quantity >= product.quantity:
+                        return redirect(self.cannot_add_to_cart_url)
                     cart_item.quantity += 1
                     cart_item.save()
+
+                # Add product to cart
                 else:
                     Cart.objects.create(user=request.user, product=product, quantity=1)
             except ValueError:
                 raise Http404
             except Product.DoesNotExist:
                 raise Http404
+        # Wrong request
         else:
-            raise Http404
+            raise Http404("Something went wrong!")
 
         return redirect(self.success_url)
 
@@ -76,7 +89,8 @@ class UserCartDeleteView(LoginRequiredMixin, TemplateView):
                 raise Http404
             except Cart.DoesNotExist:
                 raise Http404
+        # wrong request
         else:
-            raise Http404
+            raise Http404("Something went wrong!")
 
         return redirect(self.success_url)
