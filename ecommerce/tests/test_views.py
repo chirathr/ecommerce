50fa4df195e361e79d72fc25c7f7ff8efd41c125
Chirath R <chirath.02@gmail.com>
2018-10-15 11:30:56 +0530

    ProductList View tests

--- ecommerce/tests/test_views.py
+++ ecommerce/tests/test_views.py
@@ -1,17 +1,95 @@
+import pytest
+from django.test import TestCase
+from django.urls import reverse
+
+from ecommerce.models import Product, ProductCategory, Cart
+from ecommerce.views import get_total_price_of_cart
+
 
 # Test ProductListView
-# Check for correct template
-# Test Empty product db models displays "No Products Found" message
-# Loads all products without any category
-# Loads correct category on passing category as GET params
-# Unknown category displays a "No Products Found" message
-# Test if banner image is added to context
-# Test if max 3 banner image is added to context
+from registration.models import User
 
-# Test get_total_price_of_cart
-# with empty cart
 
-# Test CardListView
+class TestProductListView(TestCase):
+    def setUp(self):
+        self.url = reverse('home')
+
+    def populate_products(self):
+        self.user = User.objects.create_user(username="Bob", password="12345")
+        self.product_1 = Product.objects.create(
+            name="Product A", price=100.0, discount_percent=15, quantity=3, rating=4)
+        self.product_2 = Product.objects.create(
+            name="Product B", price=50.0, discount_percent=10, quantity=2, rating=5)
+
+    def test_template_is_product_list(self):
+        """
+        Test is the template used is 'product_list.html.haml'
+        :return:
+        """
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, template_name='ecommerce/product_list.html.haml')
+
+    def test_template_on_with_empty_db(self):
+        """
+        Test Empty product db models displays "No Products Found" message
+        """
+        response = self.client.get(self.url)
+        self.assertContains(response, 'No products')
+
+    def test_products_are_shown(self):
+        self.populate_products()
+        response = self.client.get(self.url)
+
+        self.assertContains(response, 'Product A')
+        self.assertContains(response, 'Product B')
+
+    def test_product_categories(self):
+        """
+        Test a get request with category parameter
+        """
+        self.populate_products()
+        self.product_1.category = ProductCategory.objects.create(name="Category1")
+        self.product_1.save()
+
+        response = self.client.get(self.url + '?category=Category1')
+        self.assertContains(response, 'Product A')
+        self.assertNotContains(response, 'Product B')
+
+    def test_wrong_category(self):
+        """
+        Test a category that is not there
+        """
+        self.populate_products()
+        response = self.client.get(self.url + '?category=Category1')
+        self.assertContains(response, 'No products')
+
+
+@pytest.mark.django_db
+def test_get_total_price_of_cart():
+    """
+    Test get_total_price_of_cart()
+    """
+    p1 = Product.objects.create(
+        name="Product A", price=100.0, discount_percent=15, quantity=3, rating=4)
+    p2 = Product.objects.create(
+        name="Product B", price=230.0, discount_percent=10, quantity=4, rating=4)
+    user = User.objects.create(username="User", password="12345")
+
+    Cart.objects.create(user=user, product=p1, quantity=2)
+    Cart.objects.create(user=user, product=p2, quantity=1)
+
+    expected_total_price = (2 * p1.discount_price) + p2.discount_price
+    assert expected_total_price == get_total_price_of_cart(Cart.objects.filter(user=user))
+
+
+@pytest.mark.django_db
+def test_get_total_price_of_cart_with_empty_cart():
+    """
+    Test empty cart on get_total_price_of_cart()
+    """
+    user = User.objects.create(username="User", password="12345")
+    assert 0 == get_total_price_of_cart(Cart.objects.filter(user=user))
+
 # Check for correct template
 # Check login required
 # context['cart_list'] loads correct cart
